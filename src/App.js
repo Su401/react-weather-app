5a1083f2dee983c28d5b3ea5e7075cdaaece6cf8
Susana Filipa Ribeiro da Silva <sfrs.iefp@alunos.ipb.pt>
2023-08-29 18:24:44 +0100

    add html

--- src/App.js
+++ src/App.js
@@ -5,23 +5,25 @@ import './App.css';
 export default function App() {
 	return (
 		<div className='App'>
-			<Weather />
-			<div>
-				<small>
-					This page was coded by{' '}
-					<a href='https://www.linkedin.com/in/susana-silva-/'>
-						Susana Silva
-					</a>
-					, is open-sourced on{' '}
-					<a href='https://github.com/Su401/react-weather-app'>
-						GitHub
-					</a>{' '}
-					and hosted on{' '}
-					<a href='https://friendly-cat-798438.netlify.app/'>
-						Netlify
-					</a>
-					.
-				</small>
+			<div className='container'>
+				<Weather />
+				<footer>
+					<small>
+						This page was coded by{' '}
+						<a href='https://www.linkedin.com/in/susana-silva-/'>
+							Susana Silva
+						</a>
+						, is open-sourced on{' '}
+						<a href='https://github.com/Su401/react-weather-app'>
+							GitHub
+						</a>{' '}
+						and hosted on{' '}
+						<a href='https://friendly-cat-798438.netlify.app/'>
+							Netlify
+						</a>
+						.
+					</small>
+				</footer>
 			</div>
 		</div>
 	);
--- src/Weather.js
+++ src/Weather.js
@@ -1,9 +1,47 @@
 import React from 'react';
+import './Weather.css';
 
 export default function Weather() {
 	return (
 		<div className='Weather'>
-			<h1>Heluu</h1>
+			<form>
+				<div className='row'>
+					<div className='col-9'>
+						<input
+							type='search'
+							placeholder='Type a city...'
+							className='formController'
+						/>
+					</div>
+					<div className='col-3'>
+						<input
+							type='submit'
+							value='Search'
+							className='btn btn-primary'
+						/>
+					</div>
+				</div>
+			</form>
+
+			<h1>City</h1>
+			<ul>
+				<li>Day 00/00</li>
+				<li>Description</li>
+			</ul>
+
+			<div className='row'>
+				<div className='col-6'>
+					<img src='' alt='description' />
+					xª
+				</div>
+				<div className='col-6'>
+					<ul>
+						<li>Precipitation: x %</li>
+						<li>Humidity: x %</li>
+						<li>Wind: x km/h</li>
+					</ul>
+				</div>
+			</div>
 		</div>
 	);
 }
