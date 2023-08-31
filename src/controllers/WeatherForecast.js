8369959a70adc514a99f3a5f62c48db51a766a9d
Susana Filipa Ribeiro da Silva <sfrs.iefp@alunos.ipb.pt>
2023-08-31 19:59:54 +0100

    add forecast  api

--- src/controllers/WeatherForecast.js
+++ src/controllers/WeatherForecast.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import '../css/WeatherForecast.css';
+import axios from 'axios';
 
 export default function WeatherForecast(props) {
+	function handleResponse(res) {
+		console.log(res.data);
+	}
+
+	const apikey = '5d4be4co359dcfb3b02ea04bt4fdc01e';
+	let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${props.data.city}&key=${apikey}&units=metric`;
+	axios.get(apiUrl).then(handleResponse);
+
 	return (
 		<div className='WeatherForecast'>
 			<div className='row'>
