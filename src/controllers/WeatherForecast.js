5d81422670c7e8a8910411e7f9d1507b5cf23755
Susana Filipa Ribeiro da Silva <sfrs.iefp@alunos.ipb.pt>
2023-09-01 15:29:34 +0100

    add day forecast

--- src/controllers/WeatherForecast.js
+++ src/controllers/WeatherForecast.js
@@ -1,30 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../css/WeatherForecast.css';
+import DayForecast from './DayController';
 import axios from 'axios';
 
 export default function WeatherForecast(props) {
+	let [loaded, setLoaded] = useState(false);
+	let [forecast, setForecast] = useState(null);
+
 	function handleResponse(res) {
-		console.log(res.data);
+		setForecast(res.data.daily);
+		setLoaded(true);
 	}
-
-	const apikey = '5d4be4co359dcfb3b02ea04bt4fdc01e';
-	let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${props.data.city}&key=${apikey}&units=metric`;
-	axios.get(apiUrl).then(handleResponse);
-
-	return (
-		<div className='WeatherForecast'>
-			<div className='row'>
-				<div className='col'>
-					<div className='forecastDay'> Day</div>
-					<div className='forecastIcon'>
-						<img src={props.data.iconUrl} alt='icon' />
-					</div>
-					<div className='forecastTemps'>
-						<span className='forecastTempMax'>X</span> |{' '}
-						<span className='forecastTempMin'>X</span>
-					</div>
-				</div>
+	if (loaded) {
+		console.log(forecast);
+		return (
+			<div className='WeatherForecast'>
+				<DayForecast data={forecast[0]} />
 			</div>
-		</div>
-	);
+		);
+	} else {
+		const apikey = '5d4be4co359dcfb3b02ea04bt4fdc01e';
+		let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${props.data.city}&key=${apikey}&units=metric`;
+		axios.get(apiUrl).then(handleResponse);
+		return null;
+	}
 }
