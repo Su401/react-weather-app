d71c964f3bb1b0300cc0cb8041d934229f5359a1
Susana Filipa Ribeiro da Silva <sfrs.iefp@alunos.ipb.pt>
2023-09-01 15:57:39 +0100

    add 5 days forecast

--- src/controllers/DateController.js
+++ src/controllers/DateController.js
@@ -24,7 +24,7 @@ export default function DisplayDate(props) {
 
 	return (
 		<div className='DisplayDate'>
-			{day} {hours}h{minutes}
+			{day} {hours}:{minutes}
 		</div>
 	);
 }
--- src/controllers/WeatherForecast.js
+++ src/controllers/WeatherForecast.js
@@ -11,17 +11,33 @@ export default function WeatherForecast(props) {
 		setForecast(res.data.daily);
 		setLoaded(true);
 	}
+	function load() {
+		const apikey = '5d4be4co359dcfb3b02ea04bt4fdc01e';
+		let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${props.data.city}&key=${apikey}&units=metric`;
+		axios.get(apiUrl).then(handleResponse);
+	}
+
 	if (loaded) {
 		console.log(forecast);
 		return (
 			<div className='WeatherForecast'>
-				<DayForecast data={forecast[0]} />
+				<div className='row'>
+					{forecast.map(function (dailyForecast, index) {
+						if (index < 5) {
+							return (
+								<div className='col' key={index}>
+									<DayForecast data={dailyForecast} />
+								</div>
+							);
+						} else {
+							return null;
+						}
+					})}
+				</div>
 			</div>
 		);
 	} else {
-		const apikey = '5d4be4co359dcfb3b02ea04bt4fdc01e';
-		let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${props.data.city}&key=${apikey}&units=metric`;
-		axios.get(apiUrl).then(handleResponse);
+		load();
 		return null;
 	}
 }
